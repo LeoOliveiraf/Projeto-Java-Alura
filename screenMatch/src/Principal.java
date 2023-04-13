d7062d4302f38d3f4af52df5c860e980641d36dd
Leonardo de Oliveira Florindo <oliveiraleo797@gmail.com>
2023-04-13 17:44:37 -0300

    Continuando projeto

--- screenMatch/src/Principal.java
+++ screenMatch/src/Principal.java
@@ -1,17 +1,17 @@
+import br.com.alura.screenmatch.modelos.Filme;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinuto = 180;
+        meuFilme.setNome("O poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinuto(180);
 
         meuFilme.exibiFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println(meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegaMedia());
     }
 }
