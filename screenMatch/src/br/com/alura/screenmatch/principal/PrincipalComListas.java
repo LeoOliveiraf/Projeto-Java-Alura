c319abe058a230f50edd51ee55ad28bcc186c540
Leonardo de Oliveira Florindo <oliveiraleo797@gmail.com>
2023-04-19 16:27:50 -0300

    Continuando o curso de POO

--- screenMatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenMatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,7 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -15,15 +15,31 @@ public class PrincipalComListas {
         Filme filmePaulo = new Filme("Dogville", 2003);
         filmePaulo.avalia(10);
         Serie lost = new Serie("Lost", 2018);
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new LinkedList<>();
         lista.add(filmePaulo);
         lista.add(meuFilme);
         lista.add(outroFilme);
         lista.add(lost);
         for (Titulo item: lista) {
             System.out.println(item.getNome());
-            Filme filme = (Filme) item;
-            System.out.println("Classificação: " +  filme.getClassificacao());
+            if(item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificação: " +  filme.getClassificacao());
+            }
+
         }
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+
+        Collections.sort(buscaPorArtista);
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
     }
 }
